b7e71a400c3bc27dde1b15c5d26af245e34abf4e
Anton Antonov <rumatoff@gmail.com>
2020-05-20 14:15:59 +0300

    added sum and number of array elements

---
+++ lesson_2/sum_and_number.js
@@ -0,0 +1,21 @@
+/*eslint strict: ["error", "global"]*/
+
+"use strict";
+
+const arr = [91, 93, 45, -67, -96, -40, 34, -96, 42, -58];
+
+function getNegative(array) {
+    return array.filter(function (item) {
+        return item < 0;
+    });
+}
+
+function sumAndNumber(array) {
+    return {
+        count: array.length,
+        sum: array.reduce(function (a, b) {
+            return a + b;
+        })
+    };
+}
+console.log(sumAndNumber(getNegative(arr)));
