2cfd8b543c9a9df284a9a96e8c2a468a5494e51b
Anton Antonov <rumatoff@gmail.com>
2020-06-16 14:36:14 +0300

    added jsDoc for charSum

--- lesson_3/word_stat.js
+++ lesson_3/word_stat.js
@@ -2,22 +2,43 @@
 
 "use strict";
 
+/**
+ *
+ * @param {string} text
+ * @returns {*|string[]}
+ */
 function getWords(text) {
     return text.split(" ");
 }
 
+/**
+ *
+ * @param {string} word
+ * @returns {number[]}
+ */
 function wordToChars(word) {
     return word.split("").map(function (letter) {
         return letter.charCodeAt(0);
     });
 }
 
+/**
+ *
+ * @param {number[]} chars
+ * @returns {number}
+ */
 function charsSum(chars) {
     return chars.reduce(function (a, b) {
         return a + b;
     }, 0);
 }
 
+
+/**
+ *
+ * @param text
+ * @return {{sum: number, word: string}[]}
+ */
 function wordStat(text) {
     return getWords(text).map(function (word) {
         return {"word": word, "sum": charsSum(wordToChars(word))};
